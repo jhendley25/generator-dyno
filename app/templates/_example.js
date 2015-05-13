7e0d6c25a2d8f4a5501e6e9eeaacfcfdd8134c27
jhendley25 <jake.hendley25@gmail.com>
2015-05-13 11:33:42 -0400

    update _example.js to use let

--- app/templates/_example.js
+++ app/templates/_example.js
@@ -1,6 +1,6 @@
 exports.welcome = function(){
-  var inputs = ["d","y","n","o"]
-  var result = inputs.map(word => word.split("")[0] ).reduce( (a,b) => a+b );
+  let inputs = ["d","y","n","o"]
+  let result = inputs.map(word => word.split("")[0] ).reduce( (a,b) => a+b );
   // because everyone loves over-engineering
   console.log(result)
 }
